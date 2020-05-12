f1cbf58928302447d4e0f8bf1b41ed41a193b1d4
Tim Work <timi.coker@admedo.com>
2020-05-12 14:55:55 +0100

    quick fix

--- src/Services.js
+++ src/Services.js
@@ -2,108 +2,109 @@ import Papa from 'papaparse';
 import moment from 'moment';
 
 export const rangeFilter = (set, range) => {
-    let filteredData;
-    if (!range) {
-        return set;
-    }
-    const endDate = moment().subtract(1, 'days').format('YYYY-MM-DD');
-    const startDate = moment().subtract(range, 'days').format('YYYY-MM-DD');
+  let filteredData;
+  if (!range) {
+    return set;
+  }
+  const endDate = moment().subtract(1, 'days').format('YYYY-MM-DD');
+  const startDate = moment().subtract(range, 'days').format('YYYY-MM-DD');
 
-    filteredData = set.filter(dataSet => {
-        let date = moment(dataSet.time).format('YYYY-MM-DD');
-        return (date >= startDate && date <= endDate);
-    });
-    return filteredData;
-}
+  filteredData = set.filter((dataSet) => {
+    let date = moment(dataSet.time).format('YYYY-MM-DD');
+    return date >= startDate && date <= endDate;
+  });
+  return filteredData;
+};
 
 export const rangeFilter2 = (set, range) => {
-    if (!range) {
-        return set;
-    }
-    let filteredData;
+  if (!range) {
+    return set;
+  }
+  let filteredData;
 
-    const endDate = moment().subtract(1, 'days').format('YYYY-MM-DD');
-    const startDate = moment().subtract(range, 'days').format('YYYY-MM-DD');
+  const endDate = moment().subtract(1, 'days').format('YYYY-MM-DD');
+  const startDate = moment().subtract(range, 'days').format('YYYY-MM-DD');
 
-    filteredData = set.filter(dataSet => {
-        let date = moment(dataSet.datetime).format('YYYY-MM-DD');
-        return (date >= startDate && date <= endDate);
-    });
-    return filteredData;
-}
+  filteredData = set.filter((dataSet) => {
+    let date = moment(dataSet.datetime).format('YYYY-MM-DD');
+    return date >= startDate && date <= endDate;
+  });
+  return filteredData;
+};
 
 export const subSetFilter = (set, subset) => {
-    if (!subset) {
-        return set;
-    }
-    let filteredData;
-    filteredData = set.filter(dataSet => {
-        return dataSet.sensor_type === subset;
-    });
-    return filteredData;
-}
-
+  if (!subset) {
+    return set;
+  }
+  let filteredData;
+  filteredData = set.filter((dataSet) => {
+    return dataSet.sensor_type === subset;
+  });
+  return filteredData;
+};
 
 function relDiff(a, b) {
-    if (a === NaN || b === NaN) {
-        return
-    }
-    return 100 * Math.abs((a - b) / ((a + b) / 2));
+  if (a === NaN || b === NaN) {
+    return;
+  }
+  return 100 * Math.abs((a - b) / ((a + b) / 2));
 }
 
 export const getAlerts = (x, y) => {
-    let amberAlert = [];
-    let redAlert = [];
+  let amberAlert = [];
+  let redAlert = [];
 
-    for (var obj in x) {
-
-        if (relDiff(x[obj].value, y[obj].value) > 1) {
-            amberAlert.push(x[obj])
-        }
-        if (relDiff(x[obj].value, y[obj].value) > 3) {
-            redAlert.push(x[obj])
-        }
+  for (var obj in x) {
+    if (relDiff(x[obj].value, y[obj].value) > 1) {
+      amberAlert.push(x[obj]);
     }
-    return { amberAlert, redAlert };
-}
+    if (relDiff(x[obj].value, y[obj].value) > 3) {
+      redAlert.push(x[obj]);
+    }
+  }
+  return { amberAlert, redAlert };
+};
 
 export const fetchCsv = () => {
-    return fetch('mock_data/weather_data.csv').then((response) => {
-        let reader = response.body.getReader();
-        let decoder = new TextDecoder('utf-8');
+  return fetch('mock_data/weather_data.csv')
+    .then((response) => {
+      let reader = response.body.getReader();
+      let decoder = new TextDecoder('utf-8');
 
-        return reader.read().then(function (result) {
-            return decoder.decode(result.value);
-        });
-    }).then(cleanData)
-}
+      return reader.read().then(function (result) {
+        return decoder.decode(result.value);
+      });
+    })
+    .then(cleanData);
+};
 
 export const fetchCsv2 = (int) => {
-    let param = '';
-    if (int === 2) {
-        param = 'mock_data/sensor_02.csv'
-    } else {
-        param = 'mock_data/sensor_01.csv'
-    }
-    return fetch(param).then((response) => {
-        let reader = response.body.getReader();
-        let decoder = new TextDecoder('utf-8');
+  let param = '';
+  if (int === 2) {
+    param = 'mock_data/sensor_02.csv';
+  } else {
+    param = 'mock_data/sensor_01.csv';
+  }
+  return fetch(param)
+    .then((response) => {
+      let reader = response.body.getReader();
+      let decoder = new TextDecoder('utf-8');
 
-        return reader.read().then(function (result) {
-            return decoder.decode(result.value);
-        });
-    }).then(cleanData)
-}
+      return reader.read().then(function (result) {
+        return decoder.decode(result.value);
+      });
+    })
+    .then(cleanData);
+};
 
 function cleanData(response) {
-    let matrixData = Papa.parse(response);
-    let dataTitles = matrixData.data[0];
-    const rows = matrixData.data.slice(1);
-    const output = rows.map(row => {
-        let obj = {};
-        dataTitles.forEach((h, i) => obj[h] = row[i]);
-        return obj;
-    });
-    return output;
+  let matrixData = Papa.parse(response);
+  let dataTitles = matrixData.data[0];
+  const rows = matrixData.data.slice(1);
+  const output = rows.map((row) => {
+    let obj = {};
+    dataTitles.forEach((h, i) => (obj[h] = row[i]));
+    return obj;
+  });
+  return output;
 }
-
--- src/SonarChart.js
+++ src/SonarChart.js
@@ -1,96 +1,92 @@
-
-import React from 'react'
+import React from 'react';
 import Chart2 from './Chart2';
-import { rangeFilter2, fetchCsv2, subSetFilter } from './Services'
+import { rangeFilter2, fetchCsv2, subSetFilter } from './Services';
 import './Chart1.css';
 import Info from './Info';
 
 class ChartWrapper extends React.Component {
-    state = {
-        data: {},
-        sensor: 1,
-        range: '30',
-        filter: '',
-        type: 'line'
-    }
+  state = {
+    data: {},
+    sensor: 1,
+    range: '30',
+    filter: '',
+    type: 'line',
+  };
 
-    async componentDidMount() {
-        const data = await fetchCsv2(this.state.sensor);
-        const filteredData = rangeFilter2(data, this.state.range);
-        this.setState({ data: filteredData });
-    }
+  async componentDidMount() {
+    const data = await fetchCsv2(this.state.sensor);
+    const filteredData = rangeFilter2(data, this.state.range);
+    this.setState({ data: filteredData });
+  }
 
-    handleRangeChange = async (e) => {
-        e.preventDefault();
-        const range = e.target.value;
-        const data = await fetchCsv2(this.state.sensor);
-        const filteredData = rangeFilter2(data, range);
-        const subset = subSetFilter(filteredData, this.state.filter);
-        this.setState({ range: range });
-        this.setState({ data: subset });
-    }
+  handleRangeChange = async (e) => {
+    e.preventDefault();
+    const range = e.target.value;
+    const data = await fetchCsv2(this.state.sensor);
+    const filteredData = rangeFilter2(data, range);
+    const subset = subSetFilter(filteredData, this.state.filter);
+    this.setState({ range: range });
+    this.setState({ data: subset });
+  };
 
-    handleSensorChnage = async (e) => {
-        e.preventDefault();
-        const sensor = e.target.value;
-        const data = await fetchCsv2(sensor);
-        const filteredData = rangeFilter2(data, this.state.range);
-        const subset = subSetFilter(filteredData, this.state.filter);
-        this.setState({ sensor: sensor });
-        this.setState({ data: subset });
-    }
+  handleSensorChnage = async (e) => {
+    e.preventDefault();
+    const sensor = e.target.value;
+    const data = await fetchCsv2(sensor);
+    const filteredData = rangeFilter2(data, this.state.range);
+    const subset = subSetFilter(filteredData, this.state.filter);
+    this.setState({ sensor: sensor });
+    this.setState({ data: subset });
+  };
 
-    handleSetChange = async (e) => {
-        e.preventDefault();
-        const filter = e.target.value;
-        const data = await fetchCsv2(this.state.sensor);
-        const filteredData = rangeFilter2(data, this.state.range);
-        const subset = subSetFilter(filteredData, filter);
-        this.setState({ filter: filter });
-        this.setState({ data: subset });
-    }
-    changeGraph(e) {
-        e.preventDefault();
-        this.setState({ type: e.target.value });
-    }
+  handleSetChange = async (e) => {
+    e.preventDefault();
+    const filter = e.target.value;
+    const data = await fetchCsv2(this.state.sensor);
+    const filteredData = rangeFilter2(data, this.state.range);
+    const subset = subSetFilter(filteredData, filter);
+    this.setState({ filter: filter });
+    this.setState({ data: subset });
+  };
+  changeGraph(e) {
+    e.preventDefault();
+    this.setState({ type: e.target.value });
+  }
 
-
-
-    render() {
-        const { data, type } = this.state;
-        return (
-            <div>
-                <div className="chart-wrapper">
-                    <Info data={data} />
-                    <h3> Sensor Data </h3>
-                    <Chart2 data={data}
-                        type={type} />
-                    <select defaultValue="30" onChange={(e) => this.handleRangeChange(e)}>
-                        <option value="7">1 Week</option>
-                        <option value="30">30 Days</option>
-                        <option value="90">90 Days</option>
-                        <option value="365">1 Year</option>
-                        <option value="">All Data</option>
-                    </select>
-                    <select defaultValue="1" onChange={(e) => this.handleSensorChnage(e)}>
-                        <option value="1">Sensor 1</option>
-                        <option value="2">Sensor 2</option>
-                    </select>
-                    <select defaultValue="" onChange={(e) => this.handleSetChange(e)}>
-                        <option value="">All Tilts</option>
-                        <option value="x_tilt">X Tilt</option>
-                        <option value="y_tilt">Y Tilt</option>
-                        <option value="z_tilt">Z Tilt</option>
-                    </select>
-                    <select defaultValue="" onChange={(e) => this.changeGraph(e)}>
-                        <option value="line">Line Graph</option>
-                        <option value="bar">Bar Graph</option>
-                        <option value="scat">Scatter Plot</option>
-                    </select>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { data, type } = this.state;
+    return (
+      <div>
+        <div className="chart-wrapper">
+          <Info data={data} />
+          <h3> Sensor Data </h3>
+          <Chart2 data={data} type={type} />
+          <select defaultValue="30" onChange={(e) => this.handleRangeChange(e)}>
+            <option value="7">1 Week</option>
+            <option value="30">30 Days</option>
+            <option value="90">90 Days</option>
+            <option value="365">1 Year</option>
+            <option value="">All Data</option>
+          </select>
+          <select defaultValue="1" onChange={(e) => this.handleSensorChnage(e)}>
+            <option value="1">Sensor 1</option>
+            <option value="2">Sensor 2</option>
+          </select>
+          <select defaultValue="" onChange={(e) => this.handleSetChange(e)}>
+            <option value="">All Tilts</option>
+            <option value="x_tilt">X Tilt</option>
+            <option value="y_tilt">Y Tilt</option>
+            <option value="z_tilt">Z Tilt</option>
+          </select>
+          <select defaultValue="" onChange={(e) => this.changeGraph(e)}>
+            <option value="line">Line Graph</option>
+            <option value="bar">Bar Graph</option>
+            <option value="scat">Scatter Plot</option>
+          </select>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ChartWrapper;
